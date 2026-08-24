bb0520f8b3c31725c5eef5270a2da774910e3b01
morlegenie <dioufmor938@gmail.com>
2026-08-24 01:53:23 +0000

    Add click event to start button for navigation

---
+++ script.js
@@ -0,0 +1,7 @@
+const startButton = document.getElementById("startButton");
+
+if (startButton) {
+    startButton.addEventListener("click", function () {
+        window.location.href = "niveaux.html";
+    });
+}
